9a6d57cca7688910fb17f27cf6094f3af77f52df
yanqiangmiffy <1185918903@qq.com>
2023-09-09 23:29:06 +0800

    update

--- make_rqa.py
+++ make_rqa.py
@@ -1,5 +1,5 @@
 import json
-
+import os
 with open('data/GoMate/demo.txt', 'r', encoding='utf-8') as f:
     print(r"\n".join(f.read().split('\n')))
 print("================================")
@@ -8,13 +8,17 @@ with open('data/GoMate/demo.json', 'r', encoding='utf-8') as f:
 
     print("{}{}{}".format(data["instruction"], data["input"], data["output"]))
 
+# with open('data/GoMate/searchgpt_retrieval_qa.json', 'r', encoding='utf-8') as f:
+#     sing_data = json.load(f)
+#
+# with open('data/GoMate/searchgpt_retrieval_qa.jsonl', 'w', encoding='utf-8') as f:
+#     instruction = {}
+#     for sample in sing_data:
+#         print(sample)
+#         f.write(json.dumps(sample, ensure_ascii=False) + '\n')
 
-
-with open('data/GoMate/searchgpt_retrieval_qa.json', 'r', encoding='utf-8') as f:
-    sing_data = json.load(f)
-
-with open('data/GoMate/searchgpt_retrieval_qa.jsonl', 'w', encoding='utf-8') as f:
-    instruction = {}
-    for sample in sing_data:
-        print(sample)
-        f.write(json.dumps(sample, ensure_ascii=False) + '\n')
+# data=[]
+# for idx,file in enumerate(os.listdir('data/GoMate/questions')):
+#     if idx>20:
+#         data.append(open(f'data/GoMate/questions/{file}','r',encoding='utf-8').read())
+# print(data)
